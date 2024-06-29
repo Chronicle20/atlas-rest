9967596e5b478a853ee49f27866607a0b62e1b10
Chronicle20 <a.chronicle.20@gmail.com>
2024-06-29 13:38:58 -0400

    Implement SliceProvider.

--- requests/provider.go
+++ requests/provider.go
@@ -25,31 +25,31 @@ func Provider[A any, M any](l logrus.FieldLogger) func(r Request[A], t Transform
 	}
 }
 
-func SliceProvider[A any, M any](l logrus.FieldLogger) func(r Request[A], t Transformer[A, M], filters ...model.Filter[M]) model.SliceProvider[M] {
-	return func(r Request[A], t Transformer[A, M], filters ...model.Filter[M]) model.SliceProvider[M] {
+func SliceProvider[A any, M any](l logrus.FieldLogger) func(r Request[[]A], t Transformer[A, M], filters ...model.Filter[M]) model.SliceProvider[M] {
+	return func(r Request[[]A], t Transformer[A, M], filters ...model.Filter[M]) model.SliceProvider[M] {
 		return func() ([]M, error) {
 			results := make([]M, 0)
-			//resp, err := r(l, span)
-			//if err != nil {
-			//	return results, err
-			//}
+			resp, err := r(l)
+			if err != nil {
+				return results, err
+			}
 
-			//for _, v := range resp.DataList() {
-			//	m, err := t(v)
-			//	if err != nil {
-			//		return nil, err
-			//	}
-			//	ok := true
-			//	for _, filter := range filters {
-			//		if !filter(m) {
-			//			ok = false
-			//			break
-			//		}
-			//	}
-			//	if ok {
-			//		results = append(results, m)
-			//	}
-			//}
+			for _, v := range resp {
+				m, err := t(v)
+				if err != nil {
+					return nil, err
+				}
+				ok := true
+				for _, filter := range filters {
+					if !filter(m) {
+						ok = false
+						break
+					}
+				}
+				if ok {
+					results = append(results, m)
+				}
+			}
 			return results, nil
 		}
 	}
